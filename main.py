813a9f71b80903c05b9e0cd7dbf554c1f115f80f
Christian Hiland <hilandchristian112@gmail.com>
2022-11-05 13:09:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ type = input("What langage are you from? ")
 
 if type == 'English':
     module.english.korean()
+    print("Here you go.")
 elif type == 'Korean':
     module.korean.english()
+    print("Here you go.")
 else:
     print("Sorry we don't have that language. :(")
