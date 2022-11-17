6aef5b5d6f4f1b01b4921710cd82b914f4309f1e
Christian Hiland <hilandchristian112@gmail.com>
2022-11-17 10:47:54 -0500

    Update about.py

--- module/about/about.py
+++ module/about/about.py
@@ -3,10 +3,11 @@ from tkinter import tk
 # Making the window & settings
 about= tk.Tk()
 about.geometry("600x400")
-main.title("About")
+about.title("About")
 # Actions
 
 # Labels
+Label(about, text="This is a Korean dictionary that I made called, PyKorean. It's coded in Python").pack
 
 # Button
 
