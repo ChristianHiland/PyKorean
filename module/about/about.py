68de96fc6b5f12c5c40dc4d916a6699fc7075dbc
Christian Hiland <hilandchristian112@gmail.com>
2022-11-17 10:44:45 -0500

    Made About info

--- module/about/about.py
+++ module/about/about.py
@@ -1,1 +1,15 @@
-import 
+from tkinter import tk
+
+# Making the window & settings
+about= tk.Tk()
+about.geometry("600x400")
+main.title("About")
+# Actions
+
+# Labels
+
+# Button
+
+# Showing the elements
+
+about.mainloop()
