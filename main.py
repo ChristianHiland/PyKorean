969b03d3e6220757fbb4c44c15b56eb35fdfd0be
Christian Hiland <hilandchristian112@gmail.com>
2022-11-17 10:53:22 -0500

    Added a quit button

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def korean():
     module.korean.english()
 def about():
    module.about.about()
+def quit():
+   main.destroy
 # The buttons
 E = Tkinter.Button(main, text ="English", command = english)
 K = Tkinter.Button(main, text = "Korean", command = korean)
 A = Tkinter.Button(main, text ="About", command = about)
+Q = Tkinter.Button(main, text ="Quit", command = quit)
 # Showing the elements.
 E.pack()
 K.pack()
 A.pack()
+Q.pack()
 # If this is not here the window will not stay opened.
 main.mainloop()
