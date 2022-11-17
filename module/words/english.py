4b15cec8a5714173e0d512cf02539d958bf96216
Christian Hiland <hilandchristian112@gmail.com>
2022-11-17 00:26:38 -0500

    Window support finished

--- module/words/english.py
+++ module/words/english.py
@@ -23,112 +23,112 @@ def korean():
     if word == 'hello':
         Label(english, text"안녕하세요").pack
     elif word == 'yes':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"네").pack
         print("네")
     elif word == 'home':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"음악").pack
         print("음악")
     elif word == 'opposite':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"반").pack
         print("반")
     elif word == 'side':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"면").pack
         print("면")
     elif word == 'dog':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"개").pack
         print("개")
     elif word == 'Mr':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"씨").pack
         print("씨")
     elif word == 'I':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"나").pack
         print("나")
     elif word == 'she':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"여자").pack
         print("여자")
     elif word == 'he':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"그").pack
         print("그")
     elif word == 'they':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"그들").pack
         print("그들")
     elif word == 'we':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"나").pack
         print("나")
     elif word == 'go':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"가요").pack
         print("가요")
     elif word == 'come':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"오요").pack
         print("오요")
     elif word == 'have':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"있요").pack
         print("있요")
     elif word == 'be':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"이요").pack
         print("이요")
     elif word == 'eat':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"먹요").pack
         print("먹요")
     elif word == 'drink':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"마시요").pack
         print("마시요")
     elif word == 'give':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"주요").pack
         print("주요")
     elif word == 'see':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"보요").pack
         print("보요")
     elif word == 'sleep':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"자요").pack
         print("자요")
     elif word == 'wake up':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"일어나요").pack
         print("일어나요")
     elif word == 'buy':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"사요").pack
         print("사요")
     elif word == 'write':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"쓰요").pack
         print("쓰요")
     elif word == 'laugh':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"우요").pack
         print("우요")
     elif word == 'cry':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"울요").pack
         print("울요")
     elif word == 'wear':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"입요").pack
         print("입요")
     elif word == 'walk':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"걸요").pack
         print("걸요")
     elif word == 'read':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"읠요").pack
         print("읠요")
     elif word == 'learn':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"배우요").pack
         print("배우요")
     elif word == 'study':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"공부하요").pack
         print("공부하요")
     elif word == 'meet':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"만나요").pack
         print("만나요")
     elif word == 'good':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"즣아요").pack
         print("즣아요")
     elif word == 'bad':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"나빠요").pack
         print("나빠요")
     elif word == 'flu':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"독감").pack
         print("독감")
     elif word == 'death':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"자망").pack
         print("자망")
     elif word == 'no':
-        Label(english, text"안녕하세요").pack
+        Label(english, text"아니요").pack
         print("아니요")
     elif word == 'tear':
         Label(english, text"눈물").pack
