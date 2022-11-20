d252eb161a9a529fa7513640ef8e1c5146643848
Christian Hiland <hilandchristian112@gmail.com>
2022-11-20 10:16:22 -0500

    Add files via upload

--- files/module/about/about.py
+++ files/module/about/about.py
@@ -1,1 +1,16 @@
+from tkinter import tk
 
+# Making the window & settings
+about= tk.Tk()
+about.geometry("400x400")
+about.title("About")
+# Actions
+
+# Labels
+A = tk.Label(about, text="This is a Korean dictionary that I made called, PyKorean. It's coded in Python").pack
+# Button
+B = tk.Button(about, text="Back", command=about.destroy)
+# Showing the elements
+A.pack()
+B.pack()
+about.mainloop()
