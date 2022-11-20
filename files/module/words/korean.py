98d92727c2a36a1ff9f74661231e4c8c596f8278
Christian Hiland <hilandchristian112@gmail.com>
2022-11-20 10:15:39 -0500

    Add files via upload

---
+++ files/module/words/korean.py
@@ -0,0 +1,236 @@
+#This is the English version where it's Korean to English
+#beeep boop
+def english():
+    word = input("단어: ")
+    if word == '안녕하세요':
+        print("Hello")
+        print("안녕하세요")
+    elif word == '네':
+        print ("Yes")
+        print("네")
+    elif word == '음악':
+        print("Home")
+        print("음악")
+    elif word == '반':
+        print("Is:")
+        print("Opposite")
+        print("반")
+        print("/////////////////////")
+    elif word == '면':
+        print("Is:")
+        print("Side")
+        print("면")
+        print("/////////////////////")
+    elif word == '개':
+        print("Is:")
+        print("Dog")
+        print("개")
+        print("/////////////////////")
+    elif word == '넘어지':
+        print("Is:")
+        print("Fall Down")
+        print("넘어지")
+        print("/////////////////////")
+    elif word == '씨':
+        print("Is:")
+        print("Mr. Ms. Mrs")
+        print("씨")
+        print("/////////////////////")
+    elif word == '나':
+        print("This Means:")
+        print("I")
+        print("나")
+        print("/////////////////////")
+    elif word == '여자':
+        print("This Means:")
+        print("She")
+        print("여자")
+        print("/////////////////////")
+    elif word == '그':
+        print("This Means:")
+        print("He")
+        print("그")
+        print("/////////////////////")
+    elif word == '그들':
+        print("This Means:")
+        print("They")
+        print("그들")
+        print("/////////////////////")
+    elif word == '나':
+        print("This Means:")
+        print("We")
+        print("나")
+        print("/////////////////////")
+    elif word == '가요':
+        print("This Means:")
+        print("Go")
+        print("가요")
+        print("/////////////////////")
+    elif word == '오요':
+        print("This Means:")
+        print("Come")
+        print("오요")
+        print("/////////////////////")
+    elif word == '있요':
+        print("This Means:")
+        print("Have")
+        print("있요")
+        print("/////////////////////")
+    elif word == '이요':
+        print("This Means:")
+        print("Be")
+        print("이요")
+        print("/////////////////////")
+    elif word == '먹요':
+        print("This Means:")
+        print("Eat")
+        print("먹요")
+        print("/////////////////////")
+    elif word == '마시요':
+        print("This Means:")
+        print("Drink")
+        print("마시요")
+        print("/////////////////////")
+    elif word == '주요':
+        print("This Means:")
+        print("Give")
+        print("주요")
+        print("/////////////////////")
+    elif word == '보요':
+        print("This Means:")
+        print("See")
+        print("보요")
+        print("/////////////////////")
+    elif word == '자요':
+        print("This Means:")
+        print("Sleep")
+        print("자요")
+        print("/////////////////////")
+    elif word == '일어나요':
+        print("This Means:")
+        print("Wake up")
+        print("일어나요")
+        print("/////////////////////")
+    elif word == '사요':
+        print("This Means:")
+        print("Buy")
+        print("사요")
+        print("/////////////////////")
+    elif word == '우요':
+        print("This Means:")
+        print("Laugh")
+        print("우요")
+        print("/////////////////////")
+    elif word == '울요':
+        print("This Means:")
+        print("Cry")
+        print("울요")
+        print("/////////////////////")
+    elif word == '입요':
+        print("This Means:")
+        print("Wear")
+        print("입요")
+        print("/////////////////////")
+    elif word == '걸요':
+        print("This Means:")
+        print("Walk")
+        print("걸요")
+        print("/////////////////////")
+    elif word == '읠요':
+        print("This Means:")
+        print("Read")
+        print("읠요")
+        print("/////////////////////")
+    elif word == '배우요':
+        print("This Means:")
+        print("Learn")
+        print("배우요")
+        print("/////////////////////")
+    elif word == '공부하요':
+        print("This Means:")
+        print("Study")
+        print("공부하요")
+        print("/////////////////////")
+    elif word == '만나요':
+        print("This Means:")
+        print("Meet")
+        print("만나요")
+        print("/////////////////////")
+    elif word == '즣아요':
+        print("This Means:")
+        print("Good")
+        print("즣아요")
+        print("/////////////////////")
+    elif word == '나빠요':
+        print("This Means:")
+        print("Bad")
+        print("나빠요")
+        print("/////////////////////")
+    elif word == '독감':
+        print("This Means:")
+        print("flu")
+        print("독감")
+        print("/////////////////////")
+    elif word == '자망':
+        print("This Means:")
+        print("Death")
+        print("자망")
+        print("/////////////////////")
+    elif word == '아니요':
+        print("This Means:")
+        print("No")
+        print("아니요")
+        print("/////////////////////")
+    elif word == '눈물':
+        print("This Means:")
+        print("Tear")
+        print("눈물")
+        print("/////////////////////")
+    elif word == '뉴스':
+        print("This Means:")
+        print("News")
+        print("뉴스")
+        print("/////////////////////")
+    elif word == '느낌':
+        print("This Means:")
+        print("Feeling")
+        print("느낌")
+        print("/////////////////////")
+    elif word == '늦요':
+        print("This Means:")
+        print("Be late")
+        print("늦요")
+        print("/////////////////////")
+    elif word == '다':
+        print("This Means:")
+        print("all")
+        print("다")
+        print("/////////////////////")
+    elif word == '다니요':
+        print("This Means:")
+        print("Attend")
+        print("다니요")
+        print("/////////////////////")
+    elif word == '다리':
+        print("This Means:")
+        print("Leg")
+        print("다리")
+        print("/////////////////////")
+    elif word == '단어':
+        print("This Means:")
+        print("Word")
+        print("단어")
+        print("/////////////////////")
+    elif word == '닫요':
+        print("This Means:")
+        print("Close")
+        print("닫요")
+        print("/////////////////////")
+    elif word == '달력':
+        print("This Means:")
+        print("Calender")
+        print("달력")
+        print("/////////////////////")
+    else:
+        print("Opps we don't have that one, check again.")
+        print("/////////////////////")
