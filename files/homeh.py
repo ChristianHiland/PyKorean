0f0b43a8a4bfcc934a5175ecec6e3e341195a688
Christian Hiland <hilandchristian112@gmail.com>
2022-11-28 08:34:38 -0500

    Add files via upload

--- files/homeh.py
+++ files/homeh.py
@@ -2,7 +2,6 @@
 import files
 from tkinter import *
 import tkinter as tk
-from .grammar import grammar
 
 def home():
    # Making the window name
@@ -16,17 +15,14 @@ def home():
    def english():
       main.destroy()
       files.english.korean()
-   def grammar():
-      grammar.grammarh()
+
    # The buttons
    E = tk.Button(main, text ="English", command = english)
    B = tk.Button(main, text="Back", command=main.quit)
-   G = tk.Button(main, text="Grammar", command= grammar)
 
    # Showing the elements.
    E.place(x=100, y=50)
    B.place(x=200, y=50)
-   G.place(x=150, y=100)
 
    # If this is not here the window will not stay opened.
    main.mainloop()
