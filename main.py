61e93644be45405117083d8ec7669003f09df3a0
Christian Hiland <hilandchristian112@gmail.com>
2022-11-27 21:30:09 -0500

    Auto-update function
    Every time you run the main file it will update the system.

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-#importing the modules and tkinter
+# This is were I import the modules.
 import tkinter as tk
 import time
 from tkinter.ttk import Progressbar
 import files
+import urllib.request
 
-# Setting up the window
+# Setting up the loading window.
 start= tk.Tk()
 start.geometry("400x200")
-start.title("Download Window")
+start.title("Update Window")
+
+# Vars
+home = "https://raw.githubusercontent.com/ChristianHiland/PyKorean/main/files/homeh.py"
+english = "https://raw.githubusercontent.com/ChristianHiland/PyKorean/main/files/words/english.py"
 
 # Actions
 def startbar():
     lang = 100
     x = 0
     while(x <lang):
+        # This moves the progressbar.
         time.sleep(0.01)
         bar["value"]+=1
         x+=1
         taskdone.set(str(int((x/lang)*100))+"%")
         text.set(str(x)+"/"+str(lang)+" loaded")
+
+        # This gets some of the files form GitHub and updates the other files.
+        filename, headers = urllib.request.urlretrieve(home, filename="files/homeh.py")
+        filename, headers = urllib.request.urlretrieve(english, filename="files/words/english.py")
+        
+        # This updates the screen to show the screen.
         start.update_idletasks()
+        
+        # If true then start the window.
         if (x==lang):
             start.destroy()
             files.homeh.home()
