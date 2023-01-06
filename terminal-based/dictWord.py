762348ba4d98eeef02680a8e15226aa424b9bde8
Christian Hiland <hilandchristian112@gmail.com>
2023-01-06 13:29:30 -0500

    Add files via upload

---
+++ terminal-based/dictWord.py
@@ -0,0 +1,196 @@
+words = {
+    # Greetings
+    "hello": "hello: 안녕하세요: annyeonghaseyo",
+    "안녕하세요": "안녕하세요: hello",
+    
+    # Answering words
+    "yes": "yes: 네: ne",
+    "네": "네: yes",
+    "no": "no: 아니요: aniyo",
+    "아니요": "아니요: no",
+    
+    # Time
+    "time": "time: 시각: sigak",
+    "시각": "시각: time",
+    "times": "times: 타임스: taimseu",
+    "타임스": "타임스: taimseu: times",
+    "day": "day: 낮: nach",
+    "낮": "낮: nach: day",
+    "Monday": "Monday: 월요일: woryoil",
+    "월요일": "월요일: Monday",
+    "Tuesday": "Tuesday: 화요일: hwayoil",
+    "화요일": "화요일: Tuesday",
+    "Wednesday": "Wednesday: 수요일: suyoil",
+    "수요일": "수요일: Wednesday",
+    "Thursday": "Thursday: 목요일: mogyoil",
+    "목요일": "목요일: Thursday",
+    "Friday": "Friday: 금요일: geumyoil",
+    "금요일": "금요일: Friday",
+    "Saturday": "Saturday: 토요일: toyoil",
+    "토요일": "토요일: Saturday",
+    "Sunday": "Sunday: 일요일: iryoil",
+    "일요일": "일요일: Sunday",
+    "hour": "hour: 시: si",
+    "시": "시: hour",
+    "minute": "minute: 분: bun",
+    "분": "분: minute",
+    "second	": "second: 초: cho",
+    "초": "초: second",
+    
+    # Things
+    "gas": "gas: 가스: gaseu",
+    "가스": "가스: gas",
+    "air": "air: 공기: gongki",
+    "공기": "공기: air",
+    "language": "language: 언어: eoneo",
+    "언어": "언어: language",
+    "word": "word: 단어: daneo",
+    "단어": "단어: word",
+    "news": "news: 뉴스: nyuseu",
+    "뉴스": "뉴스: news",
+    "vocabulary": "vocabulary: 어휘: eohwi",
+    "어휘": "어휘: vocabulary",
+
+    # Places
+    "country": "country: 국가: gukga",
+    "국가": "국가: country",
+    "store": "store: 가게: gake",
+    "가게": "가게: store",
+    "at": "at: ~에: ~e",
+    "에": "에: at",
+    "Australia": "Australia: 호주: hoju",
+    "호주": "호주: Australia",
+    "USA": "USA: 미국: miguk",
+    "US": "US: 미국: miguk",
+    "미국": "미국: USA",
+
+    
+    # Movement
+    "back": "back: 뒤: dwi",
+    "뒤": "뒤: back",
+    "go": "go: 가: ga",
+    "가": "가: go",
+    "away": "away: 떨어져 있는: ddeoleojyeo ssneun",
+    "떨어져 있는": "떨어져 있는: away",
+    "close": "close: 닫요: dad",
+    "닫요": "닫요: close",
+    
+    # Actions
+    "take": "take: 테이크: teipeu",
+    "테이크": "테이크: take",
+    "read": "read: 읠: euil",
+    "읠": "읠: read",
+    "learn": "learn: 배우: baeu",
+    "배우": "배우: learn",
+    "study": "study: 공부하: gongbuha",
+    "공부하": "공부하요: study",
+    "meet": "meet: 만나: manna",
+    "만나요": "만나요: meet",
+    
+    # Pronouns & People
+    "you": "you: 너: ne",
+    "너": "너: you",
+    "mr": "Mr, Ms, Mrs: 씨: ssi",
+    "씨": "씨: Mr, Ms, Mrs",
+    "i": "I: 나: na",
+    "나": "나: I",
+    "she": "she: 여자: yeocha",
+    "여자": "여자: she",
+    "he": "he: 그: geu",
+    "그": "그: he",
+    "they": "they: 그들: geudeul",
+    "그들": "그들: they",
+    "we": "we: 나: na",
+    "나": "나: we",
+    "all": "all: 다: da",
+    "다": "다: all",
+    "german": "german: 독일 사람: donil saram",
+    "독일 사람": "독일 사람: german",
+    "person": "person: 사람: saram",
+    "사람": "사람: person",
+    "foreign": "foreign: 외국의: winunui",
+    "외국의": "외국의; foreign",
+    
+    # Asking words
+    "how": "how?: 어떻게?: eoddeohge",
+    "어떻게?": "어떻게: how?",
+    "what?": "what?: 무엇을?: mueoseur?",
+    "무엇을": "무엇을?: what?",
+    "who": "who?: 누가?: nuga?",
+    "누가": "누가: who",
+    "why?": "why?: 왜?: wae?",
+    "왜": "왜: why",
+    "where?": "where?: 어디로?: eodiro?",
+    "어디로": "어디로: where",
+    
+    # Other
+    "to": "to: 에게: ege",
+    "에게": "에게: to",
+    "many": "many: 많은: manheun",
+    "많은": "많은: many",
+    
+    # Feel
+    "cold": "cold: 추운: chun",
+    "추운": "추운: cold",
+    "alone": "alone: 홀로: hodlo",
+    "홀로": "홀로: alone",
+    "bad": "bad: 나빠요: nabba",
+    "나빠요": "나빠요: bad",
+    "good": "good: 즣아요: cheuha",
+    "즣아요": "즣아요: good",
+    
+    # Animals
+    "cat": "cat: 고양이: goyeongi",
+    "고양이": "고양이: cat",
+    "dog": "dog: 개: gae",
+    "개": "개: dog",
+    "fish": "fish: 생선: saengseon",
+    "생선": "생선: fish",
+
+    # Languages
+    "languages": "Languages: 언어: eoneo",
+    "언어": "언어: languages",
+    "Korean": "Korean: 한국어: hangugeo",
+    "한국어": "한국어: Korean",
+    "English": "English: 영어: yeongeo",
+    "영어": "영어: English",
+    "Japanese": "Japanese: 일본어: ilboneo",
+    "일본어": "일본어: Japanese"
+}
+# The greetings
+greetings = {
+    "how are you": "How are you?: 안녕하십니까?: annyeonghasibnigga?",
+    "안녕하십니까": "안녕하십니까?: How are you?"
+}
+
+# The numbers
+numbers = {
+    # One
+    "one": "One: 하나: hana",
+    "1": "one: 하나: hana",
+    "하나": "하나: 1: one",
+
+    # Two
+    "two": "two: 둘: dol",
+    "2": "two: 둘: dol",
+    "둘": "둘: two"
+}
+numberss = {
+    # One
+    "one": "One: 일: il",
+    "1": "One: 일: il",
+    "일": "일: 1: one",
+
+    # Two
+    "two": "two: 이: ee",
+    "2": "two: 이: ee",
+    "이": "이: two"
+}
+
+# The sets
+sets = {
+    "1": "The ㄴ (n) is pronounced n, but shorted. ㅇ is not pronounced, unless it's at the end of a word thing, then it will be pronounced ng. ㄷ is pronounced as t, but in the ending it's pronounced as d, ㅅ is pronounced as s. ㄱ is sometimes pronounced as k or a g.",
+    "1a": "Sorry I haven't added this yet. :( ㄴ ㅇ ㄷ ㅅ ㄱ",
+    "2": "Sorry I haven't added this yet. :(",
+    "2a": "Sorry I haven't added this yet. :(",
+}
